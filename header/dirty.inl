9191fb1e769bd160c17aa41d7aed76d2e57292cd
Renato Nobre <renatonobre@MacBook-Pro-de-Renato-2.local>
2017-07-02 11:58:44 -0300

    Documenting existing code

--- header/dirty.inl
+++ header/dirty.inl
@@ -27,8 +27,18 @@ using namespace std;
 /* ----------- Definition of all functions of the program ----------- */
 
 /* -- Functions to show data on command terminal -- */
+void printcVertex();
+void printdVertex();
+void printGrafo();
 
 /* -- Functions to create and populate the base graph -- */
+int readFile();
+void add_rEdge(string r_desc, string r_date, float r_value, int dVertex, int cVertex);
+int add_cVertex(string c_name, string c_id);
+int add_dVertex(string d_name, string d_state, string d_party);
+int nameExistence(int deputyOrCompany, string name);
+int edgeExistence(string r_desc, string nome, int d_index);
+
 
 /* -- Global variables definitions -- */
 typedef struct cVertex{
@@ -46,15 +56,12 @@ typedef struct rEdge{
 
 }t_rEdge;
 
-struct dVertex{
+typedef struct dVertex{
     string name;
     string state;
     string party;
     vector<rEdge> gastos;
-};
-
-
-
+}t_dVertex;
 
 pair< vector<dVertex>, vector<cVertex> > GRAPH;
 
--- src/dirty.cpp
+++ src/dirty.cpp
@@ -11,31 +11,61 @@
 
 #include "dirty.inl"
 
+/**
+ *  Print the Graph in the following format
+ *  1 - DEPUTY_NAME / DEPUTY_STATE / DEPUTY_PARTY
+ *      - TYPE_OF_EXPANSE AT THE COMPANY COMPANY_NAME / COMPANY_ID:
+ *            - EXPANSE_DATE R$EXPANSE_VALUE
+ *
+ * @return void
+ */
 void printGrafo(){
+    /*print deputy vertex*/
     for(int i = 0; i < GRAPH.first.size();i++){
         cout << i+1 << " - " << GRAPH.first[i].name << " / " << GRAPH.first[i].state << " / " << GRAPH.first[i].party << endl;
+        /*print company vertex and edge name*/
         for(int j = 0; j < GRAPH.first[i].gastos.size();j++){
-            cout << "\t-" << GRAPH.first[i].gastos[j].tipoT << " na empresa " << GRAPH.first[i].gastos[j].empresa.name << "/" << GRAPH.first[i].gastos[j].empresa.id << ": " << endl;
+            cout << "\t-" << GRAPH.first[i].gastos[j].tipoT << " na empresa " << GRAPH.first[i].gastos[j].empresa.name << " / " << GRAPH.first[i].gastos[j].empresa.id << ": " << endl;
+            /*print edge values*/
             for(int k = 0; k < GRAPH.first[i].gastos[j].transac.size();k++){
                 cout << "\t\t- " << GRAPH.first[i].gastos[j].transac[k].first << "  R$" << GRAPH.first[i].gastos[j].transac[k].second << endl;
             }
         }
     }
-
 }
 
+/**
+ *  Print Deputies vertexes as a list of name / state / party
+ *
+ * @return void
+ */
 void printdVertex(){
     for(int i = 0; i < GRAPH.first.size();i++){
         cout << i+1 << " - " << GRAPH.first[i].name << " " << GRAPH.first[i].state << " " << GRAPH.first[i].party << endl;
     }
 }
 
+/**
+ *  Print company vertexes as a list of name and id
+ *
+ * @return void
+ */
 void printcVertex(){
     for(int i = 0; i < GRAPH.second.size();i++){
         cout << i+1 << " - " << GRAPH.second[i].name << " " << GRAPH.second[i].id << endl;
     }
 }
 
+/**
+ * Look for the existence of a edge.
+ *
+ *  @param r_desc the name of the edge being looked
+ *  @param nome the name of the company vertex
+ *  @param d_index the index of the deputy
+ *
+ * @return int i represents the index of the edge being looked
+ * or -1 representing that there is no such edge
+ */
 int edgeExistence(string r_desc, string nome, int d_index){
 
     for(int i = 0; i < GRAPH.first[d_index].gastos.size();i++){
@@ -47,6 +77,16 @@ int edgeExistence(string r_desc, string nome, int d_index){
     return -1;
 }
 
+/**
+ *  Look for the existence of a vertex.
+ *
+ *  @param deputyOrCompany int value representing if it is a deputy or
+ *      company being looked
+ *  @param name string the name of the deputy or company
+ *
+ * @return int i represents the index of the vertex being looked
+ * or -1 representing that there is no such vertex
+ */
 int nameExistence(int deputyOrCompany, string name){
     if(deputyOrCompany == 0){
         for(int i = 0; i < GRAPH.first.size();i++){
@@ -65,6 +105,15 @@ int nameExistence(int deputyOrCompany, string name){
     return -1;
 }
 
+/**
+ * Add a deputy vertex on the graph
+ *
+ *  @param r_desc the name of the edge being looked
+ *  @param nome the name of the company vertex
+ *  @param d_index the index of the deputy
+ *
+ * @return int indexOfNode represents the index of the vertex
+ */
 int add_dVertex(string d_name, string d_state, string d_party){
     int indexOfNode;
 
@@ -82,6 +131,15 @@ int add_dVertex(string d_name, string d_state, string d_party){
 
 }
 
+/**
+ * Add a company vertex on the graph
+ *
+ *  @param r_desc the name of the edge being looked
+ *  @param nome the name of the company vertex
+ *  @param d_index the index of the deputy
+ *
+ * @return int indexOfNode represents the index of the vertex
+ */
 int add_cVertex(string c_name, string c_id){
     int indexOfNode;
 
@@ -98,17 +156,30 @@ int add_cVertex(string c_name, string c_id){
     return indexOfNode;
 }
 
+/**
+ * Create an edge between a given deputy vertex and a company vertex
+ *
+ *  @param r_desc the name of the expanse of edge
+ *  @param r_date the date of the expanse
+ *  @param r_value the value of the expanse
+ *  @param dVertex the index of the deputy vertex
+ *  @param cVertex the index of the company vertex
+ *
+ * @return int 0 represents good exit, -1 represents bad exit
+ */
 void add_rEdge(string r_desc, string r_date, float r_value, int dVertex, int cVertex){
     int indexOfNode;
 
     indexOfNode = edgeExistence(r_desc, GRAPH.second[cVertex].name, dVertex);
 
+    /* If expanse type edge does not exist, create one for it*/
     if(indexOfNode == -1){
         struct rEdge edge;
         edge.tipoT = r_desc;
         edge.transac.push_back(make_pair(r_date, r_value));
         edge.empresa = GRAPH.second[cVertex];
         GRAPH.first[dVertex].gastos.push_back(edge);
+    /* Else, put the date and value of the expanse in the correspondent edge */
     } else {
         GRAPH.first[dVertex].gastos[indexOfNode].transac.push_back(make_pair(r_date, r_value));
     }
@@ -117,7 +188,11 @@ void add_rEdge(string r_desc, string r_date, float r_value, int dVertex, int cVe
 
 
 
-
+/**
+ * Read the text file and create the GRAPH.
+ *
+ * @return int 0 represents good exit, -1 represents bad exit
+ */
 int readFile(){
     FILE *pF;
     float r_value;
@@ -144,8 +219,6 @@ int readFile(){
         add_rEdge(sr_desc, sr_date, r_value, d_index, c_index);
 
     }
-    // printdVertex();
-    // printcVertex();
 
     printGrafo();
     fclose(pF);
@@ -164,21 +237,7 @@ int readFile(){
  */
 int main(){
 
-  string header, line;
-
   readFile();
-
-
-  // ifstream infile();
-  //
-  // getline(infile, line);
-  // header = line;
-  //
-  // while (getline(infile, line))
-  // {
-  //   break;
-  // }
-  // cout << header << endl << line << endl;
 
   return 0;
 }
